ce18a8d44f6a3682ee1682400e2bc9181e0dfa8d
Jesse Rodriguez <wellpeng97@gmail.com>
2024-09-20 16:01:46 +0800

    fix: type issus

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -15,20 +15,4 @@ export async function register() {
       process.env.CRYPTO_PRIVATE_KEY = fs.readFileSync(path.join(process.cwd(), 'privateKey.pem'), 'utf8')
     } catch (e) {}
   }
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    const fs = await import('node:fs')
-    const path = await import('node:path')
-    try {
-      const publicKey = fs.readFileSync(path.join(process.cwd(), 'publicKey.pem'), 'utf8')
-      if (!publicKey) throw new Error()
-      process.env.CRYPTO_PUBLIC_KEY = publicKey
-    } catch (e) {
-      console.error('CRYPTO_PUBLIC_KEY is missing')
-      process.exit(1)
-    }
-
-    try {
-      process.env.CRYPTO_PRIVATE_KEY = fs.readFileSync(path.join(process.cwd(), 'privateKey.pem'), 'utf8')
-    } catch (e) {}
-  }
 }
